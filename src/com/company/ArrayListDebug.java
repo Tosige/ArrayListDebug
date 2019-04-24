47f335766a85b0ee1187e488d61a8fa23640647d
Tosin Ige <tosin.ige@gmail.com>
2019-04-24 11:54:14 -0400

    initial commit

---
+++ src/com/company/ArrayListDebug.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.*;
+
+//DEBUGGED - class ArrayListDemo {
+public class ArrayListDebug {
+    //class ArrayListDemo {
+    public static void main(String[] args) {
+
+// create an array list
+// DEBUGGED - ArrayList al = new ArrayList();
+        ArrayList<String> al = new ArrayList<String>();
+
+        System.out.println("Initial size of al: " +
+                al.size());
+// add elements to the array list
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1, "A2");
+//DEBUGGED - System.out.println("Size of al after additions: " +
+//al.sizes());
+        System.out.println("Size of al after additions: " +
+                al.size());
+// display the array list
+        System.out.println("Contents of al: " + al);
+// Remove elements from the array list
+        al.remove("F");
+        al.remove("G");
+        al.remove(2);
+        System.out.println("Size of al after deletions: " +
+                al.size());
+        System.out.println("Contents of al: " + al);
+    }
+}
+
